977a984edabe7facff7327ba855974b00645d583
Johannes Eiglsperger <je@eigsys.de>
2024-02-09 10:09:56 +0100

    Split integration test and example

--- cachecontrol_integration_test.go
+++ cachecontrol_integration_test.go
@@ -107,28 +107,3 @@ func TestNew(t *testing.T) {
 		})
 	}
 }
-
-func ExampleNew() {
-	router := gin.Default()
-
-	router.Use(cachecontrol.New(cachecontrol.Config{
-		MustRevalidate:       true,
-		NoCache:              false,
-		NoStore:              false,
-		NoTransform:          false,
-		Public:               true,
-		Private:              false,
-		ProxyRevalidate:      true,
-		MaxAge:               cachecontrol.Duration(30 * time.Minute),
-		SMaxAge:              nil,
-		Immutable:            false,
-		StaleWhileRevalidate: cachecontrol.Duration(2 * time.Hour),
-		StaleIfError:         cachecontrol.Duration(2 * time.Hour),
-	}))
-
-	router.GET("/", func(ginCtx *gin.Context) {
-		ginCtx.String(http.StatusOK, "Hello, Gopher!")
-	})
-
-	_ = router.Run()
-}
